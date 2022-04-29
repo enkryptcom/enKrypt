6e8eacdd842b13114b745416e7382759b8b647b5
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2022-04-29 16:54:22 -0700

    feat: ✨ add default accounts

--- packages/extension/src/libs/utils/initialize-wallet.ts
+++ packages/extension/src/libs/utils/initialize-wallet.ts
@@ -1,5 +1,18 @@
 import KeyRing from "@/libs/keyring/keyring";
+import EthereumNetworks from "@/providers/ethereum/networks";
+import PolkadotNetworks from "@/providers/polkadot/networks";
 export default async (mnemonic: string, password: string): Promise<void> => {
   const kr = new KeyRing();
-  return kr.init(mnemonic, password);
+  await kr.init(mnemonic, password);
+  await kr.unlock(password);
+  await kr.saveNewAccount({
+    basePath: EthereumNetworks.ethereum.basePath,
+    name: "Account 1",
+    type: EthereumNetworks.ethereum.signer[0],
+  });
+  await kr.saveNewAccount({
+    basePath: PolkadotNetworks.polkadot.basePath,
+    name: "Account 2",
+    type: PolkadotNetworks.polkadot.signer[0],
+  });
 };
