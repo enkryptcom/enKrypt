6d7cb33f81b717126d6009af763fd76ed3aad3cc
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2022-07-13 16:37:31 -0700

    devop: update all packages

--- packages/extension/src/ui/action/main.ts
+++ packages/extension/src/ui/action/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import * as filters from "./utils/filters";
 import Vue3Lottie from "vue3-lottie";
 import "@polkadot/wasm-crypto/initOnlyAsm";
+
 const app = createApp(App);
 
 app.use(router).use(Vue3Lottie);
