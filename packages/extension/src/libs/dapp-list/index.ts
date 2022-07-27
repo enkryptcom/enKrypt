247d37c43cba3f2103bc4991594a2ba5b54f347a
andrewdahn <andrewahnjr@gmail.com>
2022-07-27 13:21:27 -0700

    devop: add dapps for other chains

--- packages/extension/src/libs/dapp-list/index.ts
+++ packages/extension/src/libs/dapp-list/index.ts
@@ -1,9 +1,15 @@
 import { DAppsItem } from "@/types/ui";
 import { NetworkNames } from "@enkryptcom/types";
 import eth from "./eth";
+import binance from "./binance";
+import matic from "./matic";
+import etc from "./etc";
 
 const lists: Partial<Record<NetworkNames, DAppsItem[]>> = {
   [NetworkNames.Ethereum]: eth,
+  [NetworkNames.Binance]: binance,
+  [NetworkNames.Matic]: matic,
+  [NetworkNames.EthereumClassic]: etc,
 };
 
 export default lists;
