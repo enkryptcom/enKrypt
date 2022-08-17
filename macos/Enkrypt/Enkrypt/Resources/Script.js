914135199ab35739e03c8d6819d0ec940cf752f3
admin <admin@admins-MacBook-Pro.local>
2022-08-17 15:18:01 -0700

    devop: setup macos build and fastlane

---
+++ macos/Enkrypt/Enkrypt/Resources/Script.js
@@ -0,0 +1,15 @@
+function show(enabled) {
+    if (typeof enabled === "boolean") {
+        document.body.classList.toggle(`state-on`, enabled);
+        document.body.classList.toggle(`state-off`, !enabled);
+    } else {
+        document.body.classList.remove(`state-on`);
+        document.body.classList.remove(`state-off`);
+    }
+}
+
+function openPreferences() {
+    webkit.messageHandlers.controller.postMessage("open-preferences");
+}
+
+document.querySelector("button.open-preferences").addEventListener("click", openPreferences);
