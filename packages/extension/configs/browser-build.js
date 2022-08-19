d6f926524c7d4f5293546d8bb468396e11108b9d
admin <admin@admins-MacBook-Pro.local>
2022-08-18 17:04:32 -0700

    fix: safari wasm error

--- packages/extension/configs/browser-build.js
+++ packages/extension/configs/browser-build.js
@@ -64,7 +64,8 @@ const setConfig = (config) => {
     const _base = args[0]["process.env"];
     args[0]["process.env"] = {
       ..._base,
-      IS_MANIFEST_V3: BROWSER === browserNames.chrome,
+      IS_MANIFEST_V3:
+        BROWSER === browserNames.chrome || BROWSER === browserNames.safari,
       PACKAGE_VERSION: JSON.stringify(package.version),
       IS_DEV: process.env.NODE_ENV === "development",
       IS_FIREFOX: BROWSER === browserNames.firefox,
