2f8cafccd80b97e3ece9e9e905c1c5871a3fe1bc
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2022-10-24 13:06:17 -0700

    devop: return null on error

--- packages/extension/src/libs/name-resolver/index.ts
+++ packages/extension/src/libs/name-resolver/index.ts
@@ -11,7 +11,9 @@ class GenericNameResolver {
   async resolveName(name: string, coins: CoinType[]): Promise<string | null> {
     let response: string | null = null;
     for (const coin of coins) {
-      response = await this.nameResolver.resolveAddress(name, coin);
+      response = await this.nameResolver
+        .resolveAddress(name, coin)
+        .catch(() => null);
       if (response) return response;
     }
     return response;
