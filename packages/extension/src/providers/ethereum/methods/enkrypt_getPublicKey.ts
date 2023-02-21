f63c86609655da93b52097c74a01cf65f35bb963
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2023-02-21 13:04:41 -0800

    fix: ledger swao

--- packages/extension/src/providers/ethereum/methods/enkrypt_getPublicKey.ts
+++ packages/extension/src/providers/ethereum/methods/enkrypt_getPublicKey.ts
@@ -15,6 +15,9 @@ const method: MiddlewareFunction = async function (
 ): Promise<void> {
   if (payload.method !== "enkrypt_getPublicKey") return next();
   else {
+    const whitelistDomains = ["localhost", "raffle.enkrypt.com"];
+    if (!whitelistDomains.includes(payload.options?.domain || "".toLowerCase()))
+      return res(getCustomError("Cannot call this method from this domain"));
     const isInitialized = await this.KeyRing.isInitialized();
     if (!isInitialized) {
       throttledOpenOnboard();
