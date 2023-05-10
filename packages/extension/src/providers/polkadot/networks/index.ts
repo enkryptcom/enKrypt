879e8df98f8cf202c53401c8fb2052e661384cfe
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2023-05-10 14:27:33 -0700

    devop: add interlay and kintsugi

--- packages/extension/src/providers/polkadot/networks/index.ts
+++ packages/extension/src/providers/polkadot/networks/index.ts
@@ -11,6 +11,8 @@ import edgNode from "./edgeware";
 import opalNode from "./unique/opal";
 import quartzNode from "./unique/quartz";
 import uniqueNode from "./unique/unique";
+import interlayNode from "./interlay/interlay";
+import kintsugiNode from "./interlay/kintsugi";
 
 export default {
   acala: acaNode,
@@ -26,4 +28,6 @@ export default {
   opal: opalNode,
   quartz: quartzNode,
   unique: uniqueNode,
+  interlay: interlayNode,
+  kintsugi: kintsugiNode,
 };
