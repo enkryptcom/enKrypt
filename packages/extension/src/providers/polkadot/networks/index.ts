66a762b1f0a0f77c7d099b54f9e2c7e0e5af094c
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2023-05-12 12:33:36 -0700

    fix: btc addresses getting saved to evm

--- packages/extension/src/providers/polkadot/networks/index.ts
+++ packages/extension/src/providers/polkadot/networks/index.ts
@@ -11,8 +11,8 @@ import edgNode from "./edgeware";
 import opalNode from "./unique/opal";
 import quartzNode from "./unique/quartz";
 import uniqueNode from "./unique/unique";
-import interlayNode from "./interlay/interlay";
-import kintsugiNode from "./interlay/kintsugi";
+// import interlayNode from "./interlay/interlay";
+// import kintsugiNode from "./interlay/kintsugi";
 
 export default {
   acala: acaNode,
@@ -28,6 +28,6 @@ export default {
   opal: opalNode,
   quartz: quartzNode,
   unique: uniqueNode,
-  interlay: interlayNode,
-  kintsugi: kintsugiNode,
+  // interlay: interlayNode,
+  // kintsugi: kintsugiNode,
 };
