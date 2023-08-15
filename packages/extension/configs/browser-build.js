baad05ff296e6ae8c5cf05032d55a3e89f89acb6
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2023-08-15 14:36:08 -0700

    devop: temp setup for opera and edge

--- packages/extension/configs/browser-build.js
+++ packages/extension/configs/browser-build.js
@@ -7,6 +7,7 @@ const BROWSER = process.env.BROWSER;
 const browserNames = {
   chrome: "chrome",
   firefox: "firefox",
+  operaedge: "opera-edge",
 };
 
 const browserConfigs = {
@@ -17,6 +18,10 @@ const browserConfigs = {
   [browserNames.firefox]: {
     manifest: "./src/manifest/manifest-firefox.json",
     background: "./src/scripts/firefox/background.ts",
+  },
+  [browserNames.operaedge]: {
+    manifest: "./src/manifest/manifest-edge-opera.json",
+    background: "./src/scripts/chrome/background.ts",
   },
 };
 
@@ -63,6 +68,7 @@ const setConfig = (config) => {
       PACKAGE_VERSION: JSON.stringify(package.version),
       IS_DEV: process.env.NODE_ENV === "development",
       IS_FIREFOX: BROWSER === browserNames.firefox,
+      IS_OPERA_EDGE: BROWSER === browserNames.operaedge,
       PREFILL_PASSWORD:
         process.env.NODE_ENV === "development"
           ? JSON.stringify("test pass")
