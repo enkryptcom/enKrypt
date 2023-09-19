9e9f0f446e523da842e29733ef302c525418dce2
Glaicon <gjpeixer@gmail.com>
2023-09-19 11:23:31 -0400

    update kadena ui signer

--- packages/extension/src/providers/kadena/ui/libs/signer.ts
+++ packages/extension/src/providers/kadena/ui/libs/signer.ts
@@ -3,10 +3,6 @@ import { HWwalletType } from "@enkryptcom/types";
 import HWwallet from "@enkryptcom/hw-wallets";
 import { SignerMessageOptions, SignerTransactionOptions } from "../types";
 import { getCustomError } from "@/libs/error";
-import {
-  payloadSignTransform,
-  signPayload,
-} from "@/providers/polkadot/libs/signing-utils";
 import sendUsingInternalMessengers from "@/libs/messenger/internal-messenger";
 import {
   isAscii,
@@ -15,51 +11,44 @@ import {
   u8aWrapBytes,
 } from "@polkadot/util";
 import { bufferToHex } from "ethereumjs-util";
+import { ExtrinsicPayload } from "@polkadot/types/interfaces";
 
 const TransactionSigner = (
   options: SignerTransactionOptions
 ): Promise<InternalOnMessageResponse> => {
   const { account, network, payload } = options;
-  // if (account.isHardware) {
-  //   const hwWallet = new HWwallet();
-  //   return hwWallet
-  //     .signTransaction({
-  //       transaction: payload,
-  //       networkName: network.name,
-  //       pathIndex: account.pathIndex.toString(),
-  //       pathType: {
-  //         basePath: account.basePath,
-  //         path: account.HWOptions!.pathTemplate,
-  //       },
-  //       wallet: account.walletType as unknown as HWwalletType,
-  //     })
-  //     .then((signature: string) => ({
-  //       result: JSON.stringify(signature),
-  //     }))
-  //     .catch((e) => {
-  //       return Promise.reject({
-  //         error: getCustomError(e.message),
-  //       });
-  //     });
-  // } else {
-  // const signMsg = signPayload(payload);
-  return sendUsingInternalMessengers({
-    method: InternalMethods.sign,
-    params: [payload, account],
-  }).then((res) => {
-    debugger;
-    if (res.error) return res;
-
-    // const signed = payloadSignTransform(
-    //   JSON.parse(res.result as string),
-    //   account.signerType,
-    //   true
-    // );
-    return {
-      result: JSON.parse(res.result as string),
-    };
-  });
-  // }
+  if (account.isHardware) {
+    const hwWallet = new HWwallet();
+    return hwWallet
+      .signTransaction({
+        transaction: payload as ExtrinsicPayload,
+        networkName: network.name,
+        pathIndex: account.pathIndex.toString(),
+        pathType: {
+          basePath: account.basePath,
+          path: account.HWOptions!.pathTemplate,
+        },
+        wallet: account.walletType as unknown as HWwalletType,
+      })
+      .then((signature: string) => ({
+        result: JSON.stringify(signature),
+      }))
+      .catch((e) => {
+        return Promise.reject({
+          error: getCustomError(e.message),
+        });
+      });
+  } else {
+    return sendUsingInternalMessengers({
+      method: InternalMethods.sign,
+      params: [payload, account],
+    }).then((res) => {
+      if (res.error) return res;
+      return {
+        result: JSON.parse(res.result as string),
+      };
+    });
+  }
 };
 
 const MessageSigner = (
