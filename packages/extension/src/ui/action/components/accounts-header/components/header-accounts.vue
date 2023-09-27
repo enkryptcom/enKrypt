846364ae75e6885df509edb5295c91dfeea1937f
Glaicon <gjpeixer@gmail.com>
2023-09-27 10:58:09 -0400

    add chain id reference in the header-accounts component

--- packages/extension/src/ui/action/components/accounts-header/components/header-accounts.vue
+++ packages/extension/src/ui/action/components/accounts-header/components/header-accounts.vue
@@ -8,6 +8,22 @@
       </div>
       <switch-arrow />
     </a>
+
+    <tooltip
+      v-if="
+        props.network.name === NetworkNames.Kadena ||
+        props.network.name === NetworkNames.KadenaTestnet
+      "
+      text="Chain ID 1"
+    >
+      <a class="chain__info" :class="{ active: active }">
+        <div class="chain__info-name">
+          <p>{{ "Chain 1" }}</p>
+        </div>
+        <switch-arrow />
+      </a>
+    </tooltip>
+
     <div class="account__actions">
       <notification
         v-if="isCopied"
@@ -62,6 +78,7 @@ import EvmAccountState from "@/providers/ethereum/libs/accounts-state";
 import BtcAccountState from "@/providers/bitcoin/libs/accounts-state";
 import SubstrateAccountState from "@/providers/polkadot/libs/accounts-state";
 import KadenaAccountState from "@/providers/kadena/libs/accounts-state";
+import { NetworkNames } from "@enkryptcom/types";
 
 const isCopied = ref(false);
 const domainState = new DomainState();
@@ -132,6 +149,7 @@ const disconnectFromDapp = async () => {
 
 <style lang="less">
 @import "~@action/styles/theme.less";
+
 .account {
   border-radius: 12px;
   width: 100%;
@@ -194,6 +212,7 @@ const disconnectFromDapp = async () => {
         line-clamp: 1;
         max-width: 230px;
       }
+
       span {
         display: block;
         font-style: normal;
@@ -204,12 +223,14 @@ const disconnectFromDapp = async () => {
         letter-spacing: 0.5px;
       }
     }
+
     svg {
       position: absolute;
       top: 10px;
       right: 4px;
     }
   }
+
   &__actions {
     height: 100%;
     width: auto;
@@ -244,6 +265,88 @@ const disconnectFromDapp = async () => {
     right: 8px;
     top: 52px;
     z-index: 141;
+  }
+}
+
+.chain {
+  border-radius: 12px;
+  width: 100%;
+  height: 56px;
+  display: block;
+  text-decoration: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  position: relative;
+  z-index: 104;
+  padding: 6px;
+  box-sizing: border-box;
+
+  &__info {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: row;
+    width: auto;
+    text-decoration: none;
+    position: relative;
+    box-sizing: border-box;
+    padding: 6px;
+    padding-right: 32px;
+    border-radius: 10px;
+    height: 44px;
+    cursor: pointer;
+    transition: background 300ms ease-in-out;
+
+    &:hover,
+    &.active {
+      background: @black007;
+    }
+
+    img {
+      width: 32px;
+      height: 32px;
+      margin-right: 12px;
+      border-radius: 50%;
+    }
+
+    &-name {
+      p {
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 20px;
+        letter-spacing: 0.25px;
+        color: @primaryLabel;
+        margin: 0;
+        white-space: nowrap;
+        -ms-text-overflow: ellipsis;
+        -o-text-overflow: ellipsis;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        -ms-line-clamp: 1;
+        -webkit-line-clamp: 1;
+        line-clamp: 1;
+        max-width: 230px;
+      }
+
+      span {
+        display: block;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 12px;
+        line-height: 16px;
+        color: @secondaryLabel;
+        letter-spacing: 0.5px;
+      }
+    }
+
+    svg {
+      position: absolute;
+      top: 10px;
+      right: 4px;
+    }
   }
 }
 </style>
