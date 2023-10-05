86e3b820d2ece71f5cd8773ab733c4007aa33898
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2023-10-05 15:11:58 -0700

    feat: kadena network

--- packages/extension/src/ui/action/views/lock-screen/index.vue
+++ packages/extension/src/ui/action/views/lock-screen/index.vue
@@ -52,10 +52,7 @@ import { sendToBackgroundFromAction } from "@/libs/messenger/extension";
 import { InternalMethods } from "@/types/messenger";
 import { computed } from "@vue/reactivity";
 import SwapLookingAnimation from "@action/icons/swap/swap-looking-animation.vue";
-import BitcoinNetworks from "@/providers/bitcoin/networks";
 import KeyRing from "@/libs/keyring/keyring";
-import { NetworkNames, WalletType } from "@enkryptcom/types";
-import { getAccountsByNetworkName } from "@/libs/utils/accounts";
 import { initAccounts } from "@/libs/utils/initialize-wallet";
 
 const emit = defineEmits<{
