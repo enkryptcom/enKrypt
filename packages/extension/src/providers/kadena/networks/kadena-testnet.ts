4eb91439222be6ace94c920d66635f1984ce4514
Takadenoshi <tasos@kadena.io>
2024-01-16 15:52:31 +0200

    Change kadena subnetworks to 20 for mainnet and testnet

--- packages/extension/src/providers/kadena/networks/kadena-testnet.ts
+++ packages/extension/src/providers/kadena/networks/kadena-testnet.ts
@@ -22,12 +22,14 @@ const kadenaOptions: KadenaNetworkOptions = {
     networkId: "testnet04",
     chainId: "1",
   },
-  subNetworks: [
-    {
-      id: "0",
-      name: "Chain 0",
-    },
-  ],
+  subNetworks: Array(20)
+    .fill("")
+    .map((_, idx) => {
+      return {
+        id: idx.toString(),
+        name: `Chain ${idx}`,
+      };
+    }),
   buyLink: "https://faucet.testnet.chainweb.com/",
   activityHandler: wrapActivityHandler(kadenaScanActivity),
   displayAddress: (address: string) => address.replace("0x", "k:"),
