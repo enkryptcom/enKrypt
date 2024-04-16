2991aa42eb02425773b111edba43fd55ac221ec0
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2024-04-15 19:04:37 -0700

    fix: overlap

--- packages/extension/src/ui/action/views/deposit/index.vue
+++ packages/extension/src/ui/action/views/deposit/index.vue
@@ -124,7 +124,6 @@ const toggleNotification = () => {
 
   &__wrap {
     width: 360px;
-    height: 420px;
     background: @white;
     box-shadow: 0px 0.5px 5px rgba(0, 0, 0, 0.039),
       0px 3.75px 11px rgba(0, 0, 0, 0.19);
