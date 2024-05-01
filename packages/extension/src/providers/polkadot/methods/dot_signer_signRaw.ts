abd4cf20e945d9172c3801bc239825ea664de550
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2024-05-01 12:32:24 -0700

    fix: dot sigs

--- packages/extension/src/providers/polkadot/methods/dot_signer_signRaw.ts
+++ packages/extension/src/providers/polkadot/methods/dot_signer_signRaw.ts
@@ -1,7 +1,7 @@
 import { MiddlewareFunction } from "@enkryptcom/types";
 import SubstrateProvider from "..";
 import { ProviderRPCRequest } from "@/types/provider";
-import { polkadotEncodeAddress } from "@enkryptcom/utils";
+import { polkadotEncodeAddress, utf8ToHex } from "@enkryptcom/utils";
 import { SignerPayloadRaw } from "@polkadot/types/types";
 import { getCustomError } from "@/libs/error";
 import { WindowPromise } from "@/libs/window-promise";
@@ -13,11 +13,16 @@ const method: MiddlewareFunction = function (
 ): void {
   if (payload.method !== "dot_signer_signRaw") return next();
   else {
+    console.log(payload);
     if (!payload.params?.length)
       return res(getCustomError("Missing Params: signer_signPayload"));
     const reqPayload = payload.params[0] as SignerPayloadRaw;
     if (reqPayload.type !== "bytes" && reqPayload.type !== "payload")
       return res(getCustomError("type is not bytes: signer_signRaw"));
+    const data =
+      reqPayload.type === "payload"
+        ? utf8ToHex(reqPayload.data)
+        : reqPayload.data;
     this.KeyRing.getAccount(polkadotEncodeAddress(reqPayload.address)).then(
       (account) => {
         const windowPromise = new WindowPromise();
@@ -26,7 +31,7 @@ const method: MiddlewareFunction = function (
             this.getUIPath(this.UIRoutes.dotSignMessage.path),
             JSON.stringify({
               ...payload,
-              params: [reqPayload.data, account],
+              params: [data, account],
             }),
             true
           )
