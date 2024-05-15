04aab9a19bd35523cadf7cdf3f784f56a2fbf7ad
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2024-05-15 11:45:10 -0700

    devop: set correct features

--- packages/extension/src/providers/common/libs/new-features.ts
+++ packages/extension/src/providers/common/libs/new-features.ts
@@ -1,6 +1,6 @@
 import { NetworkNames } from "@enkryptcom/types";
 
-const newNetworks = [NetworkNames.Kadena, NetworkNames.Amplitude];
-const newSwaps = [NetworkNames.MaticZK, NetworkNames.Arbitrum];
+const newNetworks = [NetworkNames.Kadena];
+const newSwaps = [NetworkNames.MaticZK, NetworkNames.Base];
 
 export { newNetworks, newSwaps };
