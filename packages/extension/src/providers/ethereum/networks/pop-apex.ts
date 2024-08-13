1154cc8fb035028962d60d0dfc6fbd5db051bd24
nickkelly1 <nick.kelly314+github@gmail.com>
2024-08-13 12:24:48 -0500

    devop: add proof of play apex network

---
+++ packages/extension/src/providers/ethereum/networks/pop-apex.ts
@@ -0,0 +1,26 @@
+import { NetworkNames } from "@enkryptcom/types";
+import { EvmNetwork, EvmNetworkOptions } from "../types/evm-network";
+import wrapActivityHandler from "@/libs/activity-state/wrap-activity-handler";
+import { EtherscanActivity } from "../libs/activity-handlers";
+import shNFTHandler from "@/libs/nft-handlers/simplehash";
+
+const apexOptions: EvmNetworkOptions = {
+  name: NetworkNames.ProofOfPlayApex,
+  name_long: "Proof of Play Apex",
+  homePage: "https://www.proofofplay.com/",
+  blockExplorerTX: "https://explorer.apex.proofofplay.com/tx/[[txHash]]",
+  blockExplorerAddr:
+    "https://explorer.apex.proofofplay.com/address/[[address]]",
+  chainID: "0x1142c",
+  isTestNetwork: false,
+  currencyName: "ETH",
+  currencyNameLong: "Ethereum",
+  node: "https://rpc.apex.proofofplay.com",
+  icon: require("./icons/apex.png"),
+  NFTHandler: shNFTHandler,
+  activityHandler: wrapActivityHandler(EtherscanActivity),
+};
+
+const apex = new EvmNetwork(apexOptions);
+
+export default apex;
--- packages/types/src/networks.ts
+++ packages/types/src/networks.ts
@@ -77,6 +77,7 @@ export enum NetworkNames {
   Sanko = "sanko",
   Degen = "degen",
   Ham = "ham",
+  ProofOfPlayApex = "ProofOfPlayApex",
 }
 
 export enum CoingeckoPlatform {
