f7ef55d2fc05746f68051b9a858c65ebdc1536a6
nickkelly1 <nick.kelly314+github@gmail.com>
2024-11-21 19:01:33 -0600

    fix: send input ui breaking with multiple periods

--- packages/extension/src/providers/ethereum/ui/send-transaction/index.vue
+++ packages/extension/src/providers/ethereum/ui/send-transaction/index.vue
@@ -486,7 +486,9 @@ const isInputsValid = computed<boolean>(() => {
   if (!isSendToken.value && !selectedNft.value.id) {
     return false;
   }
-  if (new BigNumber(sendAmount.value).gt(assetMaxValue.value)) return false;
+  const sendAmountBigNumber = new BigNumber(sendAmount.value)
+  if (sendAmountBigNumber.isNaN()) return false
+  if (sendAmountBigNumber.gt(assetMaxValue.value)) return false;
   if (gasCostValues.value.REGULAR.nativeValue === '0') return false;
   return true;
 });
--- packages/extension/src/providers/solana/ui/send-transaction/index.vue
+++ packages/extension/src/providers/solana/ui/send-transaction/index.vue
@@ -399,7 +399,9 @@ const isInputsValid = computed<boolean>(() => {
   if (!isSendToken.value && !selectedNft.value.id) {
     return false;
   }
-  if (new BigNumber(sendAmount.value).gt(assetMaxValue.value)) return false;
+  const sendAmountBigNumber = new BigNumber(sendAmount.value);
+  if (sendAmountBigNumber.isNaN()) return false;
+  if (sendAmountBigNumber.gt(assetMaxValue.value)) return false;
   return true;
 });
 
