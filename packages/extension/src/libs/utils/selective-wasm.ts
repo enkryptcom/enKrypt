1dc149f88e0312f3af5a48960d38df7b0a62c38b
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2025-03-19 11:01:15 -0700

    devop: keyring lock issue

--- packages/extension/src/libs/utils/selective-wasm.ts
+++ packages/extension/src/libs/utils/selective-wasm.ts
@@ -2,5 +2,5 @@ import { createWasm } from '@polkadot/wasm-crypto-init/asm';
 import { initBridge } from '@polkadot/wasm-crypto/init';
 
 if (__IS_SAFARI__) {
-  initBridge(createWasm);
+  // initBridge(createWasm);
 }
