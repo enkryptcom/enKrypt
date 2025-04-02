1d1ed495b48f1926cf894c451c6978eed0712d54
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-04-02 11:36:41 -0700

    devop: revert

--- packages/extension/src/providers/ethereum/networks/celo.ts
+++ packages/extension/src/providers/ethereum/networks/celo.ts
@@ -3,6 +3,7 @@ import { CoingeckoPlatform, NetworkNames } from '@enkryptcom/types';
 import { EvmNetwork, EvmNetworkOptions } from '../types/evm-network';
 import { EtherscanActivity } from '../libs/activity-handlers';
 import wrapActivityHandler from '@/libs/activity-state/wrap-activity-handler';
+import assetsInfoHandler from '@/providers/ethereum/libs/assets-handlers/assetinfo-mew';
 
 const celoOptions: EvmNetworkOptions = {
   name: NetworkNames.Celo,
@@ -18,6 +19,7 @@ const celoOptions: EvmNetworkOptions = {
   icon,
   coingeckoID: 'celo',
   coingeckoPlatform: CoingeckoPlatform.Celo,
+  assetsInfoHandler,
   activityHandler: wrapActivityHandler(EtherscanActivity),
 };
 
