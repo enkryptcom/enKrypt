c46ac731a3a244f72ab08bccfbe7e98b3fec7c1f
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2025-04-25 10:34:52 -0700

    devop: ts fixes

--- packages/extension/src/providers/common/libs/new-features.ts
+++ packages/extension/src/providers/common/libs/new-features.ts
@@ -1,6 +1,6 @@
 import { NetworkNames } from '@enkryptcom/types';
 
-const newNetworks = [];
+const newNetworks: NetworkNames[] = [];
 const newSwaps: NetworkNames[] = [];
 
 export { newNetworks, newSwaps };
