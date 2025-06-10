13638c03fb4a35687ebeeab53d47b7a7f7481287
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2025-06-10 11:53:00 -0700

    fix: node urls

--- packages/extension/src/providers/ethereum/networks/vic.ts
+++ packages/extension/src/providers/ethereum/networks/vic.ts
@@ -15,7 +15,7 @@ const tomoOptions: EvmNetworkOptions = {
   isTestNetwork: false,
   currencyName: 'VIC',
   currencyNameLong: 'Viction',
-  node: 'wss://ws.tomochain.com',
+  node: 'wss://ws.viction.xyz',
   icon,
   coingeckoID: 'tomochain',
   basePath: "m/44'/889'/0'/0",
