ba30e5a85b9f88fb0d4c90d0e174a9b0bb7d6f71
Alexandru Popenta <alexandru.p9913@gmail.com>
2025-06-27 17:26:08 +0300

    wip: mvx provider

---
+++ packages/extension/src/providers/multiversx/index.ts
@@ -0,0 +1,78 @@
+import PublicKeyRing from '@/libs/keyring/public-keyring';
+
+import getUiPath from '@/libs/utils/get-ui-path';
+import {
+  BackgroundProviderInterface,
+  ProviderName,
+  ProviderRPCRequest,
+} from '@/types/provider';
+import getRequestProvider from '@enkryptcom/request';
+import { MiddlewareFunction, OnMessageResponse } from '@enkryptcom/types';
+import EventEmitter from 'events';
+import Middlewares from './methods';
+import Networks from './networks/index';
+import { MultiversXNetwork } from './types/mvx-network';
+import UIRoutes from './ui/routes/names';
+
+class MultiversXProvider
+  extends EventEmitter
+  implements BackgroundProviderInterface
+{
+  UIRoutes = UIRoutes;
+  toWindow: (message: string) => void;
+  network: MultiversXNetwork;
+  requestProvider: any;
+  middlewares: MiddlewareFunction[];
+  namespace: ProviderName;
+  KeyRing: PublicKeyRing;
+
+  constructor(
+    toWindow: (message: string) => void,
+    network: MultiversXNetwork = Networks.multiversx,
+  ) {
+    super();
+    this.network = network;
+    this.toWindow = toWindow;
+    this.setMiddleWares();
+    this.requestProvider = getRequestProvider('', this.middlewares);
+    this.requestProvider.on('notification', (notif: any) => {
+      this.sendNotification(JSON.stringify(notif));
+    });
+    this.namespace = ProviderName.multiversx;
+    this.KeyRing = new PublicKeyRing();
+  }
+
+  private setMiddleWares(): void {
+    this.middlewares = Middlewares.map(mw => mw.bind(this));
+  }
+
+  setRequestProvider(network: MultiversXNetwork): void {
+    this.network = network;
+    this.requestProvider.changeNetwork(network.node);
+  }
+
+  request(request: ProviderRPCRequest): Promise<OnMessageResponse> {
+    return this.requestProvider
+      .request(request)
+      .then((res: any) => ({
+        result: JSON.stringify(res),
+      }))
+      .catch((e: { message: any }) => ({
+        error: JSON.stringify(e.message),
+      }));
+  }
+
+  async sendNotification(notif: string): Promise<void> {
+    return this.toWindow(notif);
+  }
+
+  async isPersistentEvent(): Promise<boolean> {
+    return false;
+  }
+
+  getUIPath(page: string): string {
+    return getUiPath(page, this.namespace);
+  }
+}
+
+export default MultiversXProvider;
---
+++ packages/extension/src/providers/multiversx/ui/routes/names.ts
@@ -0,0 +1,22 @@
+export default {
+  mvxSign: {
+    path: 'mvx-sign',
+    name: 'mvxSign',
+    component: {},
+  },
+  mvxSendTransaction: {
+    path: 'mvx-send-transaction',
+    name: 'mvxSendTransaction',
+    component: {},
+  },
+  mvxConnectDApp: {
+    path: 'mvx-connect-dapp',
+    name: 'mvxConnectDApp',
+    component: {},
+  },
+  mvxHWVerify: {
+    path: 'mvx-hw-verify',
+    name: 'mvxHWVerify',
+    component: {},
+  },
+};
