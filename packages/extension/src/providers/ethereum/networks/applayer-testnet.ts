5248b4724656280e09c9b06a3b0ab9a8ed09c25b
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2025-08-06 13:34:31 -0700

    devop: lint

--- packages/extension/src/providers/ethereum/networks/applayer-testnet.ts
+++ packages/extension/src/providers/ethereum/networks/applayer-testnet.ts
@@ -1,22 +1,23 @@
 import icon from './icons/applayer.webp';
-import { EvmNetwork, EvmNetworkOptions } from "../types/evm-network";
-import { NetworkNames } from "@enkryptcom/types";
-import wrapActivityHandler from "@/libs/activity-state/wrap-activity-handler";
+import { EvmNetwork, EvmNetworkOptions } from '../types/evm-network';
+import { NetworkNames } from '@enkryptcom/types';
+import wrapActivityHandler from '@/libs/activity-state/wrap-activity-handler';
 import { EtherscanActivity } from '../libs/activity-handlers';
 
 const netOptions: EvmNetworkOptions = {
-    name: NetworkNames.AppLayerTestnet,
-    name_long: "AppLayer Testnet",
-    homePage: "https://applayer.com",
-    blockExplorerTX: "https://testnet-explorer.applayer.com/tx/[[txHash]]",
-    blockExplorerAddr: "https://testnet-explorer.applayer.com/address/[[address]]",
-    chainID: '0x1264a',
-    isTestNetwork: true,
-    currencyName: "APPL",
-    currencyNameLong: "AppLayer",
-    node: 'https://testnet-api.applayer.com',
-    icon,
-    activityHandler: wrapActivityHandler(EtherscanActivity),
+  name: NetworkNames.AppLayerTestnet,
+  name_long: 'AppLayer Testnet',
+  homePage: 'https://applayer.com',
+  blockExplorerTX: 'https://testnet-explorer.applayer.com/tx/[[txHash]]',
+  blockExplorerAddr:
+    'https://testnet-explorer.applayer.com/address/[[address]]',
+  chainID: '0x1264a',
+  isTestNetwork: true,
+  currencyName: 'APPL',
+  currencyNameLong: 'AppLayer',
+  node: 'https://testnet-api.applayer.com',
+  icon,
+  activityHandler: wrapActivityHandler(EtherscanActivity),
 };
 
 const appLayerTestnet = new EvmNetwork(netOptions);
