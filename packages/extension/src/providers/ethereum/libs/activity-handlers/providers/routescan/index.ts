56a2bb18e14a3b9968bfae1133a535bf6b0d804a
Ruslan <ruslan.rsh.8@gmail.com>
2025-09-08 16:21:06 +0200

    feat: routescan provider

--- packages/extension/src/providers/ethereum/libs/activity-handlers/providers/routescan/index.ts
+++ packages/extension/src/providers/ethereum/libs/activity-handlers/providers/routescan/index.ts
@@ -16,34 +16,36 @@ const getAddressActivity = async (
   endpoint: string,
   chainId: string,
 ): Promise<EthereumRawInfo[]> => {
+  const limit = 50;
   const decimalChainId = parseInt(chainId, 16);
-  const url = `${endpoint}/api/evm/all/transactions?includedChainIds=${decimalChainId}&limit=30&fromAddresses=${address}&toAddresses=${address}`;
-  return fetch(url)
-    .then(res => res.json())
-    .then(res => {
-      if (!res.items) return [];
-      const results = res.items as RoutescanTxType[];
-      const newResults = results.map(tx => {
-        const rawTx: EthereumRawInfo = {
-          blockHash: tx.blockHash,
-          blockNumber: numberToHex(tx.blockNumber),
-          contractAddress: tx.to?.isContract ? tx.to.id : '',
-          data: '0x0',
-          effectiveGasPrice: numberToHex(tx.gasPrice),
-          from: tx.from.id,
-          to: tx.to?.id === '' ? null : tx.to?.id,
-          gas: numberToHex(tx.gasLimit),
-          gasUsed: numberToHex(tx.gasUsed),
-          nonce: numberToHex(tx.txIndex),
-          status: tx.status,
-          transactionHash: tx.txHash,
-          value: numberToHex(tx.value),
-          timestamp: new Date(tx.timestamp).getTime(),
-        };
-        return rawTx;
-      });
-      return newResults.slice(0, 50) as EthereumRawInfo[];
-    });
+  const url = `${endpoint}/api/evm/all/transactions?includedChainIds=${decimalChainId}&limit=${limit}&sort=desc&fromAddresses=${address}&toAddresses=${address}`;
+  const response = await fetch(url);
+  if (!response.ok) return [];
+
+  const res = await response.json();
+  if (!res.items) return [];
+
+  const results = res.items as RoutescanTxType[];
+  const newResults = results.map(tx => {
+    const rawTx: EthereumRawInfo = {
+      blockHash: tx.blockHash,
+      blockNumber: numberToHex(tx.blockNumber),
+      contractAddress: tx.to?.isContract ? tx.to.id : '',
+      data: '0x0',
+      effectiveGasPrice: numberToHex(tx.gasPrice),
+      from: tx.from.id,
+      to: tx.to?.id === '' ? null : tx.to?.id,
+      gas: numberToHex(tx.gasLimit),
+      gasUsed: numberToHex(tx.gasUsed),
+      nonce: numberToHex(tx.txIndex),
+      status: tx.status,
+      transactionHash: tx.txHash,
+      value: numberToHex(tx.value),
+      timestamp: new Date(tx.timestamp).getTime(),
+    };
+    return rawTx;
+  });
+  return newResults.slice(0, limit) as EthereumRawInfo[];
 };
 
 export default async (
