bf1d1d21e70d47a68dcd8b0f07081e5d211928b3
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2025-11-05 12:10:36 -0800

    fix: remove trailing slashes from NetworkEndpoints URLs

--- packages/extension/src/providers/ethereum/libs/activity-handlers/providers/routescan/configs.ts
+++ packages/extension/src/providers/ethereum/libs/activity-handlers/providers/routescan/configs.ts
@@ -1,8 +1,8 @@
 import { NetworkNames } from '@enkryptcom/types';
 
 const NetworkEndpoints: Record<string, string> = {
-  [NetworkNames.Nibiru]: 'https://routescan.io/',
-  [NetworkNames.NibiruTestnet]: 'https://testnet.routescan.io/',
+  [NetworkNames.Nibiru]: 'https://routescan.io',
+  [NetworkNames.NibiruTestnet]: 'https://testnet.routescan.io',
 };
 
 export { NetworkEndpoints };
